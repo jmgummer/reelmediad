802255f276d0b027ac3707dfab10d2f9acc02c67
steveoyugi <steveoyugi@gmail.com>
2014-08-19 12:05:29 +0300

    IMG Viewer

--- protected/views/swf/view.php
+++ protected/views/swf/view.php
@@ -1,6 +1,5 @@
 <?php $this->breadcrumbs=array('Image Viewer'); ?>
 <div class="row-fluid clearfix">
-<header role="heading"><h2>Image Viewer</h2></header>
 <?php
 
 echo '<img src="http://192.168.0.60/reelmediad/conversions/'.$png_file.'" width="100%" />';
