9f582fc4126ee4c97f74a57bb6af8ea571be1287
steveoyugi <steveoyugi@gmail.com>
2014-09-17 10:53:58 +0300

    Changes to SWF Viewer, move location of companies mentioned

--- protected/components/Swfviewer.php
+++ protected/components/Swfviewer.php
@@ -15,11 +15,12 @@ class Swfviewer{
     <br><a href="http://www.reelforge.com/reelmedia/files/pdf/'.$array->file.'"><i class="fa fa-file-pdf-o"></i> Download PDF File</a> 
     <br><a href="'.Yii::app()->createUrl("swf/image", array("file"=>$array->file)).'"><i class="fa fa-file-image-o"></i> Download JPG Image</a> 
     <br><a href="'.Yii::app()->createUrl("swf/crop", array("file"=>$array->file)).'"><i class="fa fa-crop"></i> Crop This Image</a> 
-    <br><a href="'.Yii::app()->createUrl("swf/highlight", array("file"=>$array->file)).'"><i class="fa fa-file-text"></i> Highlight Page Section</a></span></p>';
+    <br><a href="'.Yii::app()->createUrl("swf/highlight", array("file"=>$array->file)).'"><i class="fa fa-file-text"></i> Highlight Page Section</a></span></p>
+    <strong class="cmention">Companies Mentioned : </strong><font class="cmention">'.substr($array->CompanyMentions, 0, -2).'</font></span>';
 	}
 
   public static function GetSwfTitles($array){
-    return '<span class="style3"><p><strong>'.$array->Title.'</strong></p><strong class="cmention">Companies Mentioned : </strong><font class="cmention">'.substr($array->CompanyMentions, 0, -2).'</font></span><br><hr class="simple"></hr>';
+    return '<span class="style3"><p><strong>'.$array->Title.'</strong></p><br><hr class="simple"></hr>';
   }
 
   public static function GetSwfFile($link)
