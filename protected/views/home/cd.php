14e7036f017376daceccd82e584be9b4424096be
steveoyugi <steveoyugi@gmail.com>
2015-02-26 10:44:10 +0300

    CSS Download Files

--- protected/views/home/cd.php
+++ protected/views/home/cd.php
@@ -5,14 +5,19 @@ $this->breadcrumbs=array('CD');
 <div class="supercenter">
 	<p>Download Your File</p>
 	<br>
-	<a href="<?=Yii::app()->createUrl("$file");?>"><i class="fa fa-file-archive-o fa-3x"></i></a>
+	<i class="fa fa-file-archive-o fa-4x"></i>
+	<a href="<?=Yii::app()->createUrl("$file");?>" class="btn btn-" >Download</a>
 </div>
 <style type="text/css">
 .supercenter{
 	font-weight: bold;
-	font-size: 24px;
+	font-size: 14px;
 	text-align: center;
 	padding: 150px 50px 50px 50px;
 }
-  
+
+.supercenter p{
+	font-weight: normal;
+	
+}
 </style>
