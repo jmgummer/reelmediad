59151c86e37a011adf50286db8baeb6231fe9dd0
steveoyugi <steveoyugi@gmail.com>
2015-02-26 10:24:57 +0300

    CD Styling

--- protected/views/home/cd.php
+++ protected/views/home/cd.php
@@ -1,5 +1,18 @@
 <?php
-
+$this->pageTitle=Yii::app()->name.' | File Zipper';
+$this->breadcrumbs=array('CD');
 ?>
-
-<a href="<?=Yii::app()->createUrl("cd/$file");?>"><i class="fa fa-file-archive-o fa-3x"></i></a>
+<div class="supercenter">
+	<p>Download Your File</p>
+	<br>
+	<a href="<?=Yii::app()->createUrl("$file");?>"><i class="fa fa-file-archive-o fa-3x"></i></a>
+</div>
+<style type="text/css">
+.supercenter{
+	font-weight: bold;
+	font-size: 24px;
+	text-align: center;
+	padding: 150px 50px 50px 50px;
+}
+  
+</style>
